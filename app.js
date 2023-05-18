d02391b2912ff992ce1e2a9e06abcc3395c776e1
Lucas g, Saint <109165110+Saint-dev@users.noreply.github.com>
2023-05-17 22:18:30 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,83 @@
-    fetch('./Assets/json/dados.json')
-      .then(response => response.json())
-      .then(data => {
-        const wallpapers = data.wallpapers;
-        const searchInput = document.getElementById('search-input');
-        const categoryButtons = document.querySelectorAll('.category-button');
-        const resultsContainer = document.getElementById('results-container');
-        let selectedCategory = null;
+fetch('./Assets/json/dados.json')
+  .then(response => response.json())
+  .then(data => {
+    const wallpapers = data.wallpapers;
+    const shuffledWallpapers = wallpapers.sort(() => Math.random() - 0.5);
+    const searchInput = document.getElementById('search-input');
+    const categoryButtons = document.querySelectorAll('.category-button');
+    const resultsContainer = document.getElementById('results-container');
+    const sectionTitle = document.getElementById('section-title'); // Adicionado
 
-        function renderWallpapers(wallpapers) {
-          resultsContainer.innerHTML = '';
+    let selectedCategory = null;
 
-          wallpapers.forEach(wallpaper => {
-            if (!selectedCategory || wallpaper.category.toLowerCase() === selectedCategory) {
-              const resultElement = document.createElement('div');
-              resultElement.className = 'wallpaper-img';
+    function renderWallpapers(wallpapers) {
+      resultsContainer.innerHTML = '';
 
-              const imageElement = document.createElement('img');
-              imageElement.src = wallpaper.image_url;
-              imageElement.className = 'zoom-image';
+      wallpapers.forEach(wallpaper => {
+        if (!selectedCategory || wallpaper.category.toLowerCase() === selectedCategory) {
+          const resultElement = document.createElement('div');
+          resultElement.className = 'wallpaper-img';
 
-              const titleElement = document.createElement('div');
-              titleElement.className = 'wallpaper-title';
-              titleElement.textContent = wallpaper.title;
+          const imageElement = document.createElement('img');
+          imageElement.src = wallpaper.image_url;
+          imageElement.className = 'zoom-image';
 
-              resultElement.appendChild(imageElement);
-              resultElement.appendChild(titleElement);
+          const titleElement = document.createElement('div');
+          titleElement.className = 'wallpaper-title';
+          titleElement.textContent = wallpaper.title;
 
-              resultElement.addEventListener('click', function() {
-                downloadWallpaper(wallpaper.image_url);
-              });
+          resultElement.appendChild(imageElement);
+          resultElement.appendChild(titleElement);
 
-              resultsContainer.appendChild(resultElement);
-            }
+          resultElement.addEventListener('click', function() {
+            downloadWallpaper(wallpaper.image_url);
           });
+
+          resultsContainer.appendChild(resultElement);
+        }
+      });
+    }
+
+    function downloadWallpaper(url) {
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = '';
+      link.click();
+    }
+
+    searchInput.addEventListener('input', function() {
+      const searchTerm = searchInput.value.toLowerCase();
+
+      const filteredWallpapers = wallpapers.filter(wallpaper => {
+        const tags = wallpaper.tags.toLowerCase();
+        return tags.includes(searchTerm);
+      });
+
+      renderWallpapers(filteredWallpapers);
+    });
+
+    categoryButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        const category = button.dataset.category.toLowerCase();
+
+        if (category === selectedCategory) {
+          selectedCategory = null;
+          button.classList.remove('active');
+          document.title = 'Pins wall';
+        } else {
+          selectedCategory = category;
+          button.classList.add('active');
+          document.title = 'Pins wall - ' + category; 
         }
 
-        function downloadWallpaper(url) {
-          const link = document.createElement('a');
-          link.href = url;
-          link.download = '';
-          link.click();
-        }
-
-        searchInput.addEventListener('input', function() {
-          const searchTerm = searchInput.value.toLowerCase();
-
-          const filteredWallpapers = wallpapers.filter(wallpaper => {
-            const tags = wallpaper.tags.toLowerCase();
-            return tags.includes(searchTerm);
-          });
-
-          renderWallpapers(filteredWallpapers);
-        });
-
-        categoryButtons.forEach(button => {
-          button.addEventListener('click', function() {
-            const category = button.dataset.category.toLowerCase();
-
-            if (category === selectedCategory) {
-              selectedCategory = null;
-              button.classList.remove('active');
-            } else {
-              selectedCategory = category;
-              button.classList.add('active');
-            }
-
-            renderWallpapers(wallpapers);
-          });
-        });
+        sectionTitle.textContent = category.toUpperCase(); 
 
         renderWallpapers(wallpapers);
-      })
-      .catch(error => {
-        console.error('Erro ao carregar o arquivo JSON:', error);
       });
+    });
+
+    renderWallpapers(wallpapers);
+  })
+  .catch(error => {
+    console.error('Erro ao carregar o arquivo JSON:', error);
+  });
